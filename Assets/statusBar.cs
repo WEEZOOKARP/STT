fe9690ad62219fa3bfec4d318a1300acf7584c56
victor657p <victor65724@gmail.com>
2025-09-03 20:08:46 +1200

    status bar icon update

--- Assets/statusBar.cs
+++ Assets/statusBar.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class statusBar : MonoBehaviour
 {
@@ -7,6 +9,11 @@ public class statusBar : MonoBehaviour
     public bool enableScroll = true;
     public float scrollSpeed = 50f;
     public healthBar hpBar;
+
+    [Header("Status icon")]
+    public Image defenceIcon;
+    public Image regenIcon;
+    public Image powerIcon;
 
     private RectTransform textRectTransform;
     private RectTransform parentRectTransform;
@@ -16,25 +23,64 @@ public class statusBar : MonoBehaviour
     private string lastDisplayText;
     private string newDisplayText;
     private Color dangerColor = Color.red;
-    private Color normalColor = Color.white;    
+    private Color normalColor = Color.white;
+
+    private float iconFillTime = 60f;
+
+    private class StatusTimer
+    {
+        public Image icon;
+        public float timer;
+        public StatusTimer(Image icon)
+        {
+            this.icon = icon;
+            this.timer = 0f;
+        }
+    }
+
+    private List<StatusTimer> activeStatusTimers = new List<StatusTimer>();
 
     void Start()
     {
         textRectTransform = statTextIntro.GetComponent<RectTransform>();
         parentRectTransform = statTextIntro.transform.parent.GetComponent<RectTransform>();
 
+        InitIcon(defenceIcon);
+        InitIcon(regenIcon);
+        InitIcon(powerIcon);
+
         UpdateStatUI();
         SetupScrolling();
     }
 
-    void Update()
+void Update()
+{
+    // --- Testing hotkeys ---
+    if (Input.GetKeyDown(KeyCode.D))  ActivateDefence();
+    if (Input.GetKeyDown(KeyCode.R))  ActivateRegen();
+    if (Input.GetKeyDown(KeyCode.P))  ActivatePower();
+
+    // --- Update active icons ---
+    if (activeStatusTimers.Count > 0)
     {
-        UpdateStatUI();
-        if (enableScroll)
-        {
-            ScrollText();
-        }
+        HandleStatusIcons();
+        statTextIntro.text = "";
     }
+    else
+    {
+        if (string.IsNullOrEmpty(statTextIntro.text))
+        {
+            statTextIntro.text = lastDisplayText;
+            SetupScrolling();
+        }
+
+        UpdateStatUI();
+
+        if (enableScroll)
+            ScrollText();
+    }
+}
+
 
     void UpdateStatUI()
     {
@@ -73,11 +119,52 @@ public class statusBar : MonoBehaviour
         Vector2 currentPos = textRectTransform.anchoredPosition;
         currentPos.x -= scrollSpeed * Time.deltaTime;
 
-        if (currentPos.x < -textWidth * 0.5)
+        if (currentPos.x < -textWidth * 0.5f)
         {
             currentPos.x = parentWidth;
         }
 
         textRectTransform.anchoredPosition = currentPos;
     }
+
+    void InitIcon(Image icon)
+    {
+        if (icon != null)
+            icon.fillAmount = 0f;
+    }
+
+    void HandleStatusIcons()
+    {
+        List<StatusTimer> finishedTimers = new List<StatusTimer>();
+
+        foreach (var timer in activeStatusTimers)
+        {
+            timer.timer += Time.deltaTime;
+            float progress = Mathf.Clamp01(timer.timer / iconFillTime);
+            timer.icon.fillAmount = 1f - progress;
+
+            if (progress >= 1f)
+                finishedTimers.Add(timer);
+        }
+
+        foreach (var timer in finishedTimers)
+        {
+            timer.icon.fillAmount = 0f;
+            activeStatusTimers.Remove(timer);
+        }
+    }
+
+    public void ActivateDefence() => ActivateStatus(defenceIcon);
+    public void ActivateRegen() => ActivateStatus(regenIcon);
+    public void ActivatePower() => ActivateStatus(powerIcon);
+
+    private void ActivateStatus(Image icon)
+    {
+        if (icon == null) return;
+
+        icon.fillAmount = 1f;
+
+        activeStatusTimers.Add(new StatusTimer(icon));
+    }
 }
+
